3ab68b77efea697ddd0574c8f23b41841661478c
Mitchell Davis <mdavis@atlas.dev>
2020-12-22 10:04:55 +1100

    Add findByStripeId() method to docs.

--- src/Facade.php
+++ src/Facade.php
@@ -2,9 +2,12 @@
 
 namespace Mitchdav\StripeIds;
 
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * @method static string hash()
  * @method static string id(string $prefix)
+ * @method static Builder findByStripeId(string $id)
  */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
